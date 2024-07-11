6daea278d026f33215988daa3e0fb8ee0d12457a
sanu-programmer <sanuk8137@gmail.com>
2024-07-11 19:18:50 +0530

    pattern hard first que

---
+++ CHAPTER_6/PATTERNHARD.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    //Question1 , print        *
+                 //          * *
+                 //        * * *
+                 //      * * * *   
+                 //    * * * * *
+    int row , col ,col1 ;
+    for(row=1 ; row<=5 ; row = row+1)
+    {
+        for(col =(5-row) ; col>=1; col =col-1)
+        {
+            cout<<""<<" ";
+        }
+        for(col1 =1 ; col1 <= row ; col1++ )
+        {
+            cout<<"*";
+        }
+        cout<<endl;                  
+}
+}
