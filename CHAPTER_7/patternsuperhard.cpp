e6a56e7d797fec29d642900501a96ecb4219887a
sanu-programmer <sanuk8137@gmail.com>
2024-07-19 14:09:01 +0530

    que 4 done!

--- CHAPTER_7/patternsuperhard.cpp
+++ CHAPTER_7/patternsuperhard.cpp
@@ -69,7 +69,49 @@ int main(){
         cout<<endl;  
     }
 
-    //Question 4 , print 
+    //Question 4 , print  * * * * * * * *     for n = 4
+                     //   * * *     * * *
+                     //   * *         * *
+                     //   *             *
+                     //   *             *
+                     //   * *         * *
+                     //   * * *     * * *
+                     //   * * * * * * * *
+    for(row =4 ; row>=1 ; row =row -1 )
+    {
+        for(col = 1 ; col<= row ; col++)
+        {
+            cout<<"*"<<" ";
+        }
+        for(col=1 ; col<= (8-2*row) ; col++)
+        {
+            cout<<"  ";
+        }
+        for(col = 1 ; col<= row ; col++)
+        {
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+    for(row =1 ; row<=4 ; row++) 
+    {
+       for(col = 1 ; col<= row ; col++)
+        {
+            cout<<"*"<<" ";
+        }
+        for(col=1 ; col<= (8-2*row) ; col++)
+        {
+            cout<<"  ";
+        }
+        for(col = 1 ; col<= row ; col++)
+        {
+            cout<<"*"<<" ";
+        }
+        cout<<endl; 
+    }
+
+
+    //                
 
                          
 
