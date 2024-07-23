ccbd5dc947d5d367e723a87f5160f9c4266065b7
sanu-programmer <sanuk8137@gmail.com>
2024-07-23 20:34:25 +0530

    while loop complete

--- CHAPTER_8/while_loop.cpp
+++ CHAPTER_8/while_loop.cpp
@@ -11,4 +11,62 @@ int main(){
         cout<<i<<" ";
         i++;
     }
+
+    //Question 2 , print table
+    int n;
+    cout<<"Enter the table: ";
+    cin>>n;
+     i = 1;
+    while(i<=10)
+    {
+        cout<<n*i<<endl;
+        i++;
+    }
+
+    //Question 3 , print factor
+    cout<<"Enter the number: ";
+    cin>>n;
+    i=1;
+    while(i<=n)
+    {
+        if(n%i==0)
+        {
+            cout<<i<<endl;
+        }
+        i++;
+
+    }
+    
+
+    //Question 4 , print even number 1 to n
+    cout<<"Enter the number: ";
+    cin>>n;
+    i=1;
+    while(i<=n)
+    {
+        if(i%2==0)
+        {
+            cout<<i<<endl;
+        }
+        i++;
+
+    }
+
+    //Question 5 , print odd number 1 to n
+    cout<<"Enter the number: ";
+    cin>>n;
+    i=1;
+    while(i<=n)
+    {
+        if(i%2!=0)
+        {
+            cout<<i<<endl;
+        }
+        i++;
+
+    }
+    
+
+
+
 }
