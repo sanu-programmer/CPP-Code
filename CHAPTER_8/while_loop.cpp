094d99ce4d099158dd7eb593739b63dc29163884
sanu-programmer <sanuk8137@gmail.com>
2024-07-23 20:01:22 +0530

    while loop started

---
+++ CHAPTER_8/while_loop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // while loop
+    //Question 1 , print 1 to 10
+    int i = 1;
+    while(i<=10)
+    {
+        cout<<i<<" ";
+        i++;
+    }
+}
