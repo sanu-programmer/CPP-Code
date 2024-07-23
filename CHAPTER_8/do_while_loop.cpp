fae4e05827c54f8a079f8f78d667e77d8eb57a92
sanu-programmer <sanuk8137@gmail.com>
2024-07-23 22:35:17 +0530

    switch statement

--- CHAPTER_8/do_while_loop.cpp
+++ CHAPTER_8/do_while_loop.cpp
@@ -47,6 +47,58 @@ int main(){
     }
 
 
+    // Switch concept 
+    i = 2;
+    switch(i)
+    {
+        case 1:
+        cout<<"Sanu"<<" ";
+        break;
+        
+        case 2:
+        cout<<"Shivam"<<" ";
+        break;
+
+        default:
+        cout<<"Som";
+    }
 
 
+    //Days of week program using switch statement
+    cout<<"Enter the day: ";
+    cin>>i;
+    switch (i)
+    {
+    case 1:
+        cout<<"Monday";/* code */
+        break;
+
+    case 2:
+         cout<<"Tuesday";
+         break;
+
+    case 3:
+         cout<<"Wednesday";
+         break;
+
+    case 4:
+         cout<<"Thursday";
+         break;
+
+    case 5:
+         cout<<"Friday";
+         break;                  
+
+    case 6:
+         cout<<"Saturday";
+         break;
+
+    case 7:
+         cout<<"Sunday";
+         break;     
+    
+    default:
+        cout<<"Invalid input";
+        break;
+    }
 }
